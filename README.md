4e7308b3e13bfcd1ea4f6f3757eced183dbe1235
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-17 09:14:14 -0300

    useState#02

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-## React Hooks
+### React Hooks
 React Hooks -> Adicionas várias funcionalidades nos (componentes Funcionais -> Componentes baseados em funções).
 
+
+## useState
 useState -> É um dos hooks mais utilizados, e um dos mais fáceis de ser aplicado. Estado é algo que é mutável, ou seja, ele pode ser alterado.
+
+Quando o estado modifica, a interface visual é atualizada.
+
+Componente não controlado -> Componente que não tem nenhum estado vínculado.
--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -5,6 +5,7 @@ import SectionTitle from '../../components/layout/SectionTitle'
 const UseState = (props) => {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("");
 
     return (
         <div className="UseState">
@@ -24,7 +25,9 @@ const UseState = (props) => {
                 </div>
             </div>
 
-            <SectionTitle title='Exercício #01'/>
+            <SectionTitle title='Exercício #02'/>
+            <input type="text" className="input" placeholder="Digite seu nome" value={name} onChange={e => setName(e.target.value)}/>
+            <h2>Seja bem vindo, {name}</h2>
         </div>
     )
 }
